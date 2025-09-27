b4f7cb2277e3588a6cc01ee66377937119a71237
UdayveerGupta <udayveer671@hotmail.com>
2025-09-27 18:55:15 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./app.css";
+import "./App.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
